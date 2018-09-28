5a0a1a30ef925dca8a3946a4523be78a0daf8d2b
11762729462@qq.com <11762729462@qq.com>
2018-09-28 16:15:13 +0800

    test1

---
+++ src/pkg/Main.java
@@ -0,0 +1,21 @@
+package pkg;
+import java.sql.*;
+public class Main {
+	public static void main(String[] args) {
+		String driverName = "com.microsoft.sqlserver.jdbc.SQLServerDriver";
+		String dbURL = "jdbc:sqlserver://localhost:1433;DatabaseName=test1";
+		String userName = "sa";
+		String userPwd = "CHERRY0926";
+		try
+		{
+			Class.forName(driverName);
+			Connection dbConn = DriverManager.getConnection(dbURL, userName, userPwd);
+			System.out.println("连接数据库成功");
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+			System.out.print("连接失败");
+		}
+	}
+}
